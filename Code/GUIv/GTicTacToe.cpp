c95f765de53bb456a82221a200ad6c8a431770cf
ChristopherBennett <chrisjwbennett@gmail.com>
2025-01-04 15:22:18 +0000

    adding files for C++ GUI version

---
+++ Code/GUIv/GTicTacToe.cpp
@@ -0,0 +1,24 @@
+/*
+ * Tic Tac Toe in C++ 
+ * with a GUI using raylib
+ *
+ * Christopher Bennett
+ *
+ * 04/01/2025
+ *
+ * */
+
+#include <iostream>
+#include "raylib.h"
+
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "We Ball" << endl;
+
+	return 0;
+}
